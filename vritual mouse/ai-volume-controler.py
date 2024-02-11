93285707b2a02bae97af25ccf5f71a585be57eac
Nitharshan-E <b3222508@gmail.com>
2024-02-11 09:43:49 +0530

    volume controll changed

--- vritual mouse/ai-volume-controler.py
+++ vritual mouse/ai-volume-controler.py
@@ -4,7 +4,7 @@ from math import hypot
 from ctypes import cast, POINTER
 from comtypes import CLSCTX_ALL
 from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
-import numpy as np 
+import numpy as np
 
 cap = cv2.VideoCapture(0)
 
